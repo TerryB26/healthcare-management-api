ee42b84c71b0e708cf8cca5fe91eff7653de0bb9
TerryB <terryb@innoble.co.za>
2024-05-26 13:32:59 +0200

    Create an appointment get function

--- server.js
+++ server.js
@@ -473,7 +473,9 @@ app.get('/api/appointments', (req, res) => {
   'join doctors d '+ 
   'on a.doctor_id = d.doctor_id '+
   'join patients p '+ 
-  'on p.patient_id = a.patient_id';
+  'on p.patient_id = a.patient_id'+
+  'join users u ';
+  'on u.user_id = p.user_id ';
 
   if(req.query.doctor_license_number) {
     sql += ' WHERE d.doctor_license_number = ' + db.escape(req.query.doctor_license_number);
